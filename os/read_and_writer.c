e7830e206cb5fb37f96c74515baec4dda1d52400
chris <chrisaliang@outlook.com>
2019-06-18 15:41:40 +0800

    read and write

--- os/read_and_writer.c
+++ os/read_and_writer.c
@@ -20,20 +20,20 @@ reader()
     while (1)
     {
         // psuedocode
-        P(mutex);
-        if (count == 0)
+        P(mutex);           // 互斥访问count变量
+        if (count == 0)     // 当第一个读进程读共享文件时
         {
-            P(rw);
+            P(rw);          // 阻止写进程写
         }
-        count++;
-        V(mutex);
+        count++;            // 读者计数器加一
+        V(mutex);           // 释放互斥变量count
         reading;
-        P(mutex);
-        count--;
-        if (count == 0)
+        P(mutex);           // 互斥访问count变量
+        count--;            // 读者计数器减一
+        if (count == 0)     // 当最后一个读进程读完共享文件
         {
-            V(rw);
+            V(rw);          // 允许写进程写
         }
-        V(mutex);
+        V(mutex);           // 释放互斥变量
     }
 }
