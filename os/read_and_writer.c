e532378aadb7348f2e0bac6937aef34387910934
chris <chrisaliang@outlook.com>
2019-06-18 15:43:25 +0800

    read and write

--- os/read_and_writer.c
+++ os/read_and_writer.c
@@ -4,6 +4,7 @@ int count = 0;
 semaphor mutex = 1;
 semaphor rw = 1;
 
+/** ***************读进程优先*****************  */
 writer()
 {
     while (1)
@@ -35,5 +36,49 @@ reader()
             V(rw);          // 允许写进程写
         }
         V(mutex);           // 释放互斥变量
+    }
+}
+
+/** **************写进程优先**************** */
+
+int count = 0;
+
+semaphor mutex = 1;
+semaphor rw = 1;
+semaphor w = 1;
+
+writer()
+{
+    while (1)
+    {
+        P(w);               // 在无写进程请求时进入
+        P(rw);
+        writing
+        V(rw);
+        V(w);               // 恢复对共享文件的访问
+    }
+}
+
+reader()
+{
+    while (1)
+    {
+        P(w);               // 在无写进程请求时进入
+        P(mutex);           // 互斥访问count变量
+        if (count == 0)     // 当第一个读进程读共享文件时
+        {
+            P(rw);          // 阻止写进程写
+        }
+        count++;
+        V(mutex);
+        V(w);               // 恢复对共享文件的访问
+        reading
+        P(mutex);
+        count--;
+        if (count==0)       // 当最后一个读进程读完共享文件
+        {
+            V(rw);          // 允许写进程写
+        }
+        V(mutex);
     }
 }
