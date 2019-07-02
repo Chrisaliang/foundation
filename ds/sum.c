922a0fb8c822deec1e2cdfcc08fdbcdb9b60a651
chris <chrisaliang@outlook.com>
2019-07-02 15:53:10 +0800

    update

--- ds/sum.c
+++ ds/sum.c
@@ -4,12 +4,12 @@ int main()
 {
     int a;
     scanf("n = %d", &a);
-    a = func(a);
+    a = sum_fun(a);
     printf("sum id %d", a);
     return 0;
 }
 
-int func(int n)
+int sum_fun(int n)
 {
     int i = 0, sum = 0;
     while (sum < n)
