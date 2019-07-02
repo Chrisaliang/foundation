22f2bf5207ec0b84218ddc95038df20393912a63
chris <chrisaliang@outlook.com>
2019-07-02 15:39:23 +0800

    update

--- ds/sum.c
+++ ds/sum.c
@@ -1,15 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
-    func(argc);
+    int a;
+    scanf("n = %d", &a);
+    a = func(a);
+    printf("sum id %d", a);
     return 0;
 }
 
-
-int func(int n){
+int func(int n)
+{
     int i = 0, sum = 0;
-    while (sum<n)
+    while (sum < n)
     {
         sum += ++i;
     }
