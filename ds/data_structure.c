67d72d2f2c4c1bce62ae0459dd9a30258fa3f122
chris <chrisaliang@outlook.com>
2019-07-04 17:15:01 +0800

    ListInsert

--- ds/data_structure.c
+++ ds/data_structure.c
@@ -1,17 +1,31 @@
+#include <stdbool.h>
 #define MaxSize 50 // 定义线性表的最大长度
 
 typedef int ElemType;
 
-typedef struct SqList
+typedef struct
 {
     ElemType *data;
     int length;
-};
+} SqList;
 
 #define InitSize 100
 
 typedef struct SeqList
 {
     ElemType *data;
-    int MaxSize, length;
+    int length;
 };
+
+bool ListInsert(SqList L, int i, ElemType e)
+{
+    if (i < 1 || i > L.length + 1)
+        return false;
+    if (L.length >= MaxSize)
+        return false;
+    for (int j = L.length; j >= i; j--)
+        L.data[j] = L.data[j - 1];
+    L.data[i - 1] = e;
+    L.length++;
+    return true;
+}
