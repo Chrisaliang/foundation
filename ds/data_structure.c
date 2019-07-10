5380cda351398a8b290ad5d019d2f104a9674afe
chris <chrisaliang@outlook.com>
2019-07-10 19:45:17 +0800

    remove x

--- ds/data_structure.c
+++ ds/data_structure.c
@@ -5,19 +5,22 @@
 
 typedef int ElemType;
 
-typedef struct {
+typedef struct
+{
     ElemType *data;
     int length;
 } SqList;
 
 #define InitSize 100
 
-typedef struct SeqList {
+typedef struct SeqList
+{
     ElemType *data;
     int length;
 } SeqList;
 
-bool ListInsert(SqList L, int i, ElemType e) {
+bool ListInsert(SqList L, int i, ElemType e)
+{
     if (i < 1 || i > L.length + 1)
         return false;
     if (L.length >= MaxSize)
@@ -29,7 +32,8 @@ bool ListInsert(SqList L, int i, ElemType e) {
     return true;
 }
 
-bool ListDelete(SqList L, int i, ElemType e) {
+bool ListDelete(SqList L, int i, ElemType e)
+{
     // 本算法实现删除顺序表中的第i个元素，并用e返回元素值
     if (i < 0 || i > L.length)
         return false;
@@ -40,22 +44,27 @@ bool ListDelete(SqList L, int i, ElemType e) {
     return true;
 }
 
-int LocateElem(SqList L, ElemType e) {
+int LocateElem(SqList L, ElemType e)
+{
     for (int i = 0; i < L.length; i++)
         if (L.data[i] == e)
             return i + 1;
     return 0;
 }
 
-bool remove_min(SqList L, ElemType e) {
-    if (L.length <= 0) {
+bool remove_min(SqList L, ElemType e)
+{
+    if (L.length <= 0)
+    {
         printf("empty list");
         return false;
     }
     e = L.data[0];
     int idx = 0;
-    for (int i = 0; i < L.length; i++) {
-        if (L.data[i] < e) {
+    for (int i = 0; i < L.length; i++)
+    {
+        if (L.data[i] < e)
+        {
             e = L.data[i];
             idx = i;
         }
@@ -65,16 +74,32 @@ bool remove_min(SqList L, ElemType e) {
     return true;
 }
 
-bool reverse_list(SqList L) {
+void reverse_list(SqList L)
+{
     ElemType temp;
-    for (int i = 0; i < L.length - 1; i++) {
+    for (int i = 0; i < L.length - 1; i++)
+    {
         temp = L.data[i];
         L.data[i] = L.data[L.length - i - 1];
         L.data[L.length - i - 1] = temp;
     }
-    return true;
 }
 
-int main() {
+void remove_x(SqList L, int x)
+{
+    int count = 0; // 记录符合条件的值的个数
+    for (int i = 0; i < L.length; i++)
+    {
+        L.data[i - k] = L.data[i]; // 将元素向前移动 count 个位置
+        if (L.data[i] == x)
+        {            // 如果元素值等于指定值
+            count++; // count计数器自增
+        }
+    }
+    L.length -= count;
+}
+
+int main()
+{
     return 0;
 }
--- ds/practice.md
+++ ds/practice.md
@@ -37,6 +37,43 @@
   > 思路：将第一个元素和最后一个元素对换，使用一个空元素，记录当前元素索引，和表长
   >
   > ~~~c
+  > void reverse_list(SqList L)
+  > {
+  >     ElemType temp;
+  >     for (int i = 0; i < L.length - 1; i++)
+  >     {
+  >         temp = L.data[i];
+  >         L.data[i] = L.data[L.length - i - 1];
+  >         L.data[L.length - i - 1] = temp;
+  >     }
+  > }
+  > ~~~
+
+* 3，对长度为n的顺序表，删除所有的值为x的数据元素，要求时间复杂度O(n)，空间复杂度O(1)
+
+  > 思路：遍历顺序表，将当前元素向前移动count个位置，当遇到符合条件的元素时，count自增
+  >
+  > ~~~c
+  > void remove_x(SqList L, int x)
+  > {
+  >     int count = 0; // 记录符合条件的值的个数
+  >     for (int i = 0; i < L.length; i++)
+  >     {
+  >         L.data[i - k] = L.data[i]; // 将元素向前移动 count 个位置
+  >         if (L.data[i] == x)
+  >         {            // 如果元素值等于指定值
+  >             count++; // count计数器自增
+  >         }
+  >     }
+  >     L.length -= count;
+  > }
+  > ~~~
+
+* 4，从有序顺序表中删除其值在给定的值s和t之间的所有元素，如果s和t不合理或顺序表空，显示错误信息并退出。
+
+  > 思路：
+  >
+  > ~~~c
   > 
   > ~~~
   >
