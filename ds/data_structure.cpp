d37e29bea5abbbe1404d6f4e62e844723f5e8ec0
chris <chrisaliang@outlook.com>
2019-08-28 20:16:20 +0800

    chapter 5 define constant

--- ds/data_structure.cpp
+++ ds/data_structure.cpp
@@ -46,6 +46,37 @@ typedef struct BiNode
     struct BiNode *lChild, *rChild;
 } BiTNode, *BiTree;
 
+#define MaxVertexNum 100
+
+typedef char VertexType;
+typedef int EdgeType;
+typedef struct
+{
+    VertexType Vex[MaxVertexNum];
+    EdgeType Edge[MaxVertexNum][MaxVertexNum];
+    int vexNum, arcNum;
+} MGraph;
+
+typedef struct ArcNode
+{
+    int adjVex;
+    struct ArcNode *next;
+    // infoType info;   // 网的边权值
+} ArcNode;
+
+typedef struct VNode
+{
+    VertexType data;
+    ArcNode *first;
+}
+VNode, AdjList[MaxVertexNum];
+
+typedef struct
+{
+    AdjList vertices;
+    int vexNum, arcNum;
+} ALGraph;
+
 /**************** Atom operate ****************/
 void InitStack(SqStack &s)
 {
@@ -471,6 +502,10 @@ void ANCESTOR(BiTree &root, BiTNode *p, BiTNode *q, BiTNode *r)
     }
     r = NULL;
 }
+
+/**************** Chapter 5 ****************/
+
+
 
 /**************** Test ****************/
 int main()
---
+++ ds/ds_define.cpp
@@ -0,0 +1,4 @@
+//
+// Created by chris on 2019/8/28. but I don't know how to write this
+//
+
